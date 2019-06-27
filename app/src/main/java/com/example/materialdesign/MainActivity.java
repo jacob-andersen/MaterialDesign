0a342984e4bb72caa1f8af97bdc56976a86904e2
jacob andersen <jacobma87@hotmail.com>
2019-06-27 17:19:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.materialdesign;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_layout);
+    }
+}
